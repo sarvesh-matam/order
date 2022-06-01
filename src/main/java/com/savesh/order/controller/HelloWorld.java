b3060fffa5024c8857377c869903be932f780d90
Nagasarveswaraiah Matam <sarvesh.matam@gmail.com>
2022-06-01 15:57:34 +0200

    Initial commit

---
+++ src/main/java/com/savesh/order/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.savesh.order.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class HelloWorld {
+
+    @GetMapping
+    public String helloWorld(){
+        return "hello order";
+    }
+}
